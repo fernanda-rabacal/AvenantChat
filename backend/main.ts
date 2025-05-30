8a470f858b0477644298a2ec212867f1952e52ca
fernanda-rabacal <nandarabacal02@hotmail.com>
2025-05-30 17:12:36 -0300

    [chore/backend-configs] - adjusting docker configurations

---
+++ backend/main.ts
@@ -0,0 +1,45 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './src/app/app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConflictInterceptor } from './src/common/errors/interceptors/conflict.interceptor';
+import { DatabaseInterceptor } from './src/common/errors/interceptors/database.interceptor';
+import { UnauthorizedInterceptor } from './src/common/errors/interceptors/unauthorized.interceptor';
+import { NotFoundInterceptor } from './src/common/errors/interceptors/not-found.interceptor';
+import { RedisIOAdapter } from './src/adapters/redis-io.adapter';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Simple blog')
+    .setDescription('Simple blog API description')
+    .setVersion('1.0')
+    .build();
+
+  const configService = app.get(ConfigService);
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.useGlobalInterceptors(
+    new ConflictInterceptor(),
+    new DatabaseInterceptor(),
+    new UnauthorizedInterceptor(),
+    new NotFoundInterceptor(),
+  );
+
+  const redisIOAdapter = new RedisIOAdapter(app, configService);
+  app.useWebSocketAdapter(redisIOAdapter);
+
+  await app.listen(process.env.PORT || 3000);
+}
+bootstrap();
